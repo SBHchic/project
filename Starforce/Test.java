2ff6f73babc6232fd3712034fc6b7f43a199df02
baekho Song <ssongqorgh@naver.com>
2020-12-19 00:16:29 +0900

    왜 master랑 hotfix랑 병합이 안됨?

--- Starforce/Test.java
+++ Starforce/Test.java
@@ -3,5 +3,6 @@ package Project.Starforce;
 public class Test {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("왜 master랑 hotfix 병합이 안되지?");
     }
 }
