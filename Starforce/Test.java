b16c1d5ee52f08ef9cf1db7472c817431075b610
baekho Song <ssongqorgh@naver.com>
2020-12-19 00:23:53 +0900

    병합 충돌용

--- Starforce/Test.java
+++ Starforce/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("왜 master랑 hotfix 병합이 안되지?");
+        System.out.println("병합 실험 중 ");
     }
 }
