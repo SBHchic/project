9cf7fc529e2e723496f20f49cb8c0d696441cf78
baekho Song <ssongqorgh@naver.com>
2021-02-03 00:06:14 +0900

    슈페리얼아이템 세부사항 + 강화 결과 데이터 + getter/setter

---
+++ icefortress/src/starforce/SuperiorStarforceDataBean.java
@@ -0,0 +1,125 @@
+package starforce;
+
+public class SuperiorStarforceDataBean {
+	
+	private long numberOfItems;
+	private int level;
+	private int fromStarforce;
+	private int starforce;
+	private byte succededCatch;
+	private long minSumUpgradePrice;
+	private long maxSumUpgradePrice;
+	private long averageSumUpgradePrice;
+	private int minUpgradeCount;
+	private int maxUpgradeCount;
+	private long averageUpgradeCount;
+	private int minDestroyCount;
+	private int maxDestroyCount;
+	private long averageDestroyCount;
+	private int minChanceTimeCount;
+	private int maxChanceTimeCount;
+	private long averageChanceTimeCount;
+	
+	public long getNumberOfItems() {
+		return numberOfItems;
+	}
+	public void setNumberOfItems(long numberOfItems) {
+		this.numberOfItems = numberOfItems;
+	}
+	public int getLevel() {
+		return level;
+	}
+	public void setLevel(int level) {
+		this.level = level;
+	}
+	public int getFromStarforce() {
+		return fromStarforce;
+	}
+	public void setFromStarforce(int fromStarforce) {
+		this.fromStarforce = fromStarforce;
+	}
+	public int getStarforce() {
+		return starforce;
+	}
+	public void setStarforce(int starforce) {
+		this.starforce = starforce;
+	}
+	public byte getSuccededCatch() {
+		return succededCatch;
+	}
+	public void setSuccededCatch(byte succededCatch) {
+		this.succededCatch = succededCatch;
+	}
+	public long getMinSumUpgradePrice() {
+		return minSumUpgradePrice;
+	}
+	public void setMinSumUpgradePrice(long minSumUpgradePrice) {
+		this.minSumUpgradePrice = minSumUpgradePrice;
+	}
+	public long getMaxSumUpgradePrice() {
+		return maxSumUpgradePrice;
+	}
+	public void setMaxSumUpgradePrice(long maxSumUpgradePrice) {
+		this.maxSumUpgradePrice = maxSumUpgradePrice;
+	}
+	public long getAverageSumUpgradePrice() {
+		return averageSumUpgradePrice;
+	}
+	public void setAverageSumUpgradePrice(long averageSumUpgradePrice) {
+		this.averageSumUpgradePrice = averageSumUpgradePrice;
+	}
+	public int getMinUpgradeCount() {
+		return minUpgradeCount;
+	}
+	public void setMinUpgradeCount(int minUpgradeCount) {
+		this.minUpgradeCount = minUpgradeCount;
+	}
+	public int getMaxUpgradeCount() {
+		return maxUpgradeCount;
+	}
+	public void setMaxUpgradeCount(int maxUpgradeCount) {
+		this.maxUpgradeCount = maxUpgradeCount;
+	}
+	public long getAverageUpgradeCount() {
+		return averageUpgradeCount;
+	}
+	public void setAverageUpgradeCount(long averageUpgradeCount) {
+		this.averageUpgradeCount = averageUpgradeCount;
+	}
+	public int getMinDestroyCount() {
+		return minDestroyCount;
+	}
+	public void setMinDestroyCount(int minDestroyCount) {
+		this.minDestroyCount = minDestroyCount;
+	}
+	public int getMaxDestroyCount() {
+		return maxDestroyCount;
+	}
+	public void setMaxDestroyCount(int maxDestroyCount) {
+		this.maxDestroyCount = maxDestroyCount;
+	}
+	public long getAverageDestroyCount() {
+		return averageDestroyCount;
+	}
+	public void setAverageDestroyCount(long averageDestroyCount) {
+		this.averageDestroyCount = averageDestroyCount;
+	}
+	public int getMinChanceTimeCount() {
+		return minChanceTimeCount;
+	}
+	public void setMinChanceTimeCount(int minChanceTimeCount) {
+		this.minChanceTimeCount = minChanceTimeCount;
+	}
+	public int getMaxChanceTimeCount() {
+		return maxChanceTimeCount;
+	}
+	public void setMaxChanceTimeCount(int maxChanceTimeCount) {
+		this.maxChanceTimeCount = maxChanceTimeCount;
+	}
+	public long getAverageChanceTimeCount() {
+		return averageChanceTimeCount;
+	}
+	public void setAverageChanceTimeCount(long averageChanceTimeCount) {
+		this.averageChanceTimeCount = averageChanceTimeCount;
+	}
+}
