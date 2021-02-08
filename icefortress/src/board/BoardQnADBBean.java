0ab432291ab1634aae5544b1a28dbd1389d85b80
baekho Song <ssongqorgh@naver.com>
2021-02-09 00:43:25 +0900

    QnA 게시판 - 댓글, 대댓글 관련 메서드 추가 + a

--- icefortress/src/board/BoardQnADBBean.java
+++ icefortress/src/board/BoardQnADBBean.java
@@ -74,18 +74,94 @@ public class BoardQnADBBean {
     	return -1; // 데이터베이스 오류
     }
     
+    public int getNextReplyWrittenID(int boardQnA_ID) { // 답글 번호를 매겨줌 (boardQnA_ReplyID)
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+    	try {
+    		conn = getConnection();
+    		
+    		pstmt = conn.prepareStatement("select boardQnA_ReplyID from boardQnA where boardQnA_ID = ? order by boardQnA_ReplyID desc");
+    		pstmt.setInt(1, boardQnA_ID);
+    		rs = pstmt.executeQuery();
+    		if (rs.next()) {
+    			return rs.getInt(2) + 1;
+    		}
+    		return 1; // 첫 번째 답글인 경우
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	} finally {
+    		if (rs != null) try { rs.close(); } catch(SQLException sqle) {}
+    		if (pstmt != null) try { pstmt.close(); } catch(SQLException sqle) {}
+            if (conn != null) try { conn.close(); } catch(SQLException sqle) {}
+    	}
+    	return -1; // 데이터베이스 오류
+    }
+    
+    public int getNextCommentWrite(int boardQnA_ID, int boardQnA_ReplyID) { // 댓글 번호를 매겨줌 (boardQnA_CommentID)
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+    	try {
+    		conn = getConnection();
+    		
+    		pstmt = conn.prepareStatement("select boardQnA_CommentID from boardQnA where boardQnA_ID = ? and boardQnA_ReplyID = ? order by boardQnA_CommentID desc");
+    		pstmt.setInt(1, boardQnA_ID);
+    		pstmt.setInt(2, boardQnA_ReplyID);
+    		rs = pstmt.executeQuery();
+    		if (rs.next()) {
+    			return rs.getInt(3) + 1;
+    		}
+    		return 1; // 첫 번째 댓글인 경우
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	} finally {
+    		if (rs != null) try { rs.close(); } catch(SQLException sqle) {}
+    		if (pstmt != null) try { pstmt.close(); } catch(SQLException sqle) {}
+            if (conn != null) try { conn.close(); } catch(SQLException sqle) {}
+    	}
+    	return -1; // 데이터베이스 오류
+    }
+    
+    public int getNextCommentReplyWrite(int boardQnA_ID, int boardQnA_ReplyID, int boardQnA_CommentID) { // 대댓글 번호를 매겨줌 (boardQnA_CommentID_Re)
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+    	try {
+    		conn = getConnection();
+    		
+    		pstmt = conn.prepareStatement("select boardQnA_CommentID_Re from boardQnA where boardQnA_ID = ? and boardQnA_ReplyID = ? and boardQnA_CommentID = ?  order by boardQnA_CommentID_Re desc");
+    		pstmt.setInt(1, boardQnA_ID);
+    		pstmt.setInt(2, boardQnA_ReplyID);
+    		pstmt.setInt(3, boardQnA_CommentID);
+    		rs = pstmt.executeQuery();
+    		if (rs.next()) {
+    			return rs.getInt(4) + 1;
+    		}
+    		return 1; // 첫 번째 대댓글인 경우
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	} finally {
+    		if (rs != null) try { rs.close(); } catch(SQLException sqle) {}
+    		if (pstmt != null) try { pstmt.close(); } catch(SQLException sqle) {}
+            if (conn != null) try { conn.close(); } catch(SQLException sqle) {}
+    	}
+    	return -1; // 데이터베이스 오류
+    }
+    
     public int write(String boardQnA_Title, String userID, String boardQnA_Content) { // 글 작성 메서드
     	Connection conn = null;
         PreparedStatement pstmt = null;
         try {
         	conn = getConnection();
         	
-        	pstmt = conn.prepareStatement("insert into boardQnA values (?, 0, 0, 0, ?, ?, ?, ?)");
+        	pstmt = conn.prepareStatement("insert into boardQnA values (?, 0, 0, 0, ?, ?, ?, ?, 1, ?)");
         	pstmt.setInt(1, getNextWrittenID());
         	pstmt.setString(2, boardQnA_Title);
         	pstmt.setString(3, userID);
         	pstmt.setString(4, getDate());
         	pstmt.setString(5, boardQnA_Content);
+        	pstmt.setString(6, "2000-01-01 00:00:00"); // 기본값 지정
         	pstmt.executeUpdate();
         	return 1; // 작성 성공
         } catch (Exception e) {
@@ -97,7 +173,86 @@ public class BoardQnADBBean {
         return -1; // 데이터베이스 오류
     }
     
-    // 글을 수정하는 메서드
+    public int writeReply(String boardQnA_Title, String userID, String boardQnA_Content, int boardQnA_ID) { // 답글 작성 메서드
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+        	conn = getConnection();
+        	
+        	pstmt = conn.prepareStatement("insert into boardQnA values (?, ?, 0, 0, ?, ?, ?, ?, 1, ?)");
+        	pstmt.setInt(1, boardQnA_ID);
+        	pstmt.setInt(2, getNextReplyWrittenID(boardQnA_ID));
+        	pstmt.setString(3, boardQnA_Title);
+        	pstmt.setString(4, userID);
+        	pstmt.setString(5, getDate());
+        	pstmt.setString(6, boardQnA_Content);
+        	pstmt.setString(7, "2000-01-01 00:00:00"); // 기본값 지정
+        	pstmt.executeUpdate();
+        	return 1; // 작성 성공
+        } catch (Exception e) {
+        	e.printStackTrace();
+        } finally {
+    		if (pstmt != null) try { pstmt.close(); } catch(SQLException sqle) {}
+            if (conn != null) try { conn.close(); } catch(SQLException sqle) {}
+    	}
+        return -1; // 데이터베이스 오류
+    }
+    
+    public int writeComment(String userID, String boardQnA_Content, int boardQnA_ID, int boardQnA_ReplyID) { // 댓글 작성 메서드
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+        	conn = getConnection();
+        	
+        	pstmt = conn.prepareStatement("insert into boardQnA values (?, ?, ?, 0, ?, ?, ?, ?, 1, ?)");
+        	pstmt.setInt(1, boardQnA_ID);
+        	pstmt.setInt(2, boardQnA_ReplyID);
+        	pstmt.setInt(3, getNextCommentWrite(boardQnA_ID, boardQnA_ReplyID));
+        	pstmt.setString(4, boardQnA_ID+","+boardQnA_ReplyID+"의 댓글");
+        	pstmt.setString(5, userID);
+        	pstmt.setString(6, getDate());
+        	pstmt.setString(7, boardQnA_Content);
+        	pstmt.setString(8, "2000-01-01 00:00:00"); // 기본값 지정
+        	pstmt.executeUpdate();
+        	return 1; // 작성 성공
+        } catch (Exception e) {
+        	e.printStackTrace();
+        } finally {
+    		if (pstmt != null) try { pstmt.close(); } catch(SQLException sqle) {}
+            if (conn != null) try { conn.close(); } catch(SQLException sqle) {}
+    	}
+        return -1; // 데이터베이스 오류
+    }
+    
+    public int writeCommentReply(String userID, String boardQnA_Content, int boardQnA_ID, int boardQnA_ReplyID, int boardQnA_CommentID) { // 대댓글 작성 메서드
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+        	conn = getConnection();
+        	
+        	pstmt = conn.prepareStatement("insert into boardQnA values (?, ?, ?, ?, ?, ?, ?, ?, 1, ?)");
+        	pstmt.setInt(1, boardQnA_ID);
+        	pstmt.setInt(2, boardQnA_ReplyID);
+        	pstmt.setInt(3, boardQnA_CommentID);
+        	pstmt.setInt(4, getNextCommentReplyWrite(boardQnA_ID, boardQnA_ReplyID, boardQnA_CommentID));
+        	pstmt.setString(5, boardQnA_ID+","+boardQnA_ReplyID+","+boardQnA_CommentID+"의 대댓글");
+        	pstmt.setString(6, userID);
+        	pstmt.setString(7, getDate());
+        	pstmt.setString(8, boardQnA_Content);
+        	pstmt.setString(9, "2000-01-01 00:00:00"); // 기본값 지정
+        	pstmt.executeUpdate();
+        	return 1; // 작성 성공
+        } catch (Exception e) {
+        	e.printStackTrace();
+        } finally {
+    		if (pstmt != null) try { pstmt.close(); } catch(SQLException sqle) {}
+            if (conn != null) try { conn.close(); } catch(SQLException sqle) {}
+    	}
+        return -1; // 데이터베이스 오류
+    }
+    
+    
+    // 글, 답글을 수정하는 메서드
     public int update(BoardQnADataBean update, String boardQnA_Title, String boardQnA_Content) {
     	Connection conn = null;
         PreparedStatement pstmt = null;
@@ -110,6 +265,62 @@ public class BoardQnADBBean {
         	pstmt.setString(3, getDate());
         	pstmt.setInt(4, update.getBoardQnA_ID());
         	pstmt.setInt(5, update.getBoardQnA_ReplyID());
+        	pstmt.executeUpdate();
+        	return 1; // 수정 성공
+        } catch (Exception e) {
+        	e.printStackTrace();
+        } finally {
+    		if (pstmt != null) try { pstmt.close(); } catch(SQLException sqle) {}
+            if (conn != null) try { conn.close(); } catch(SQLException sqle) {}
+    	}
+        return -1; // 데이터베이스 오류
+    }
+    
+    // 글, 답글을 삭제하는 메서드
+    public int delete(int boardQnA_ID, int boardQnA_ReplyID) {
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        try {
+        	conn = getConnection();
+        	
+        	pstmt = conn.prepareStatement("select from boardQnA where boardQnA_ID = ? and boardQnA_ReplyID = ?"); // 그 글의 댓글도 같이 불러옴
+        	pstmt.setInt(1, boardQnA_ID);
+        	pstmt.setInt(2, boardQnA_ReplyID);
+        	rs = pstmt.executeQuery();
+        	while(rs.next()) {
+        		pstmt = conn.prepareStatement("update boardQnA set available = 0, boardQnA_DeleteReg_Date = ? where boardQnA_ID = ? and boardQnA_ReplyID = ? and boardQnA_CommentID = ? and boardQnA_CommentID_Re = ?");
+        		pstmt.setString(1, getDate());
+        		pstmt.setInt(2, rs.getInt("boardQnA_ID"));
+        		pstmt.setInt(3, rs.getInt("boardQnA_ReplyID"));
+        		pstmt.setInt(4, rs.getInt("boardQnA_CommentID")); // 그 글의 댓글
+        		pstmt.setInt(5, rs.getInt("boardQnA_CommentID_Re")); // 그 글의 대댓글
+        		pstmt.executeUpdate(); // 그 글의 댓글, 대댓글도 같이 삭제됨
+        	}
+        	return 1; // 수정 성공
+        } catch (Exception e) {
+        	e.printStackTrace();
+        } finally {
+        	if (rs != null) try { rs.close(); } catch(SQLException sqle) {}
+    		if (pstmt != null) try { pstmt.close(); } catch(SQLException sqle) {}
+            if (conn != null) try { conn.close(); } catch(SQLException sqle) {}
+    	}
+        return -1; // 데이터베이스 오류
+    }
+    
+    // 코멘트를 삭제하는 메서드
+    public int deleteComment(BoardQnADataBean comment) {
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        try {
+        	conn = getConnection();
+        	
+        	pstmt = conn.prepareStatement("update boardQnA set available = 0, boardQnA_DeleteReg_Date = ? where boardQnA_ID = ? and boardQnA_ReplyID = ? and boardQnA_CommentID = ? and boardQnA_CommentID_Re = ?"); // 그 글의 댓글도 같이 불러옴
+        	pstmt.setString(1, getDate());
+        	pstmt.setInt(2, comment.getBoardQnA_ID());
+        	pstmt.setInt(3, comment.getBoardQnA_ReplyID());
+        	pstmt.setInt(4, comment.getBoardQnA_CommentID());
+        	pstmt.setInt(5, comment.getBoardQnA_CommentID_Re());
         	pstmt.executeUpdate();
         	return 1; // 수정 성공
         } catch (Exception e) {
@@ -139,7 +350,7 @@ public class BoardQnADBBean {
     	        ResultSet rs2 = null;
     	        try {
     	        	conn2 = getConnection();
-    	        	String SQL2 = "select * from boardQnA where boardQnA_ID = ? and boardQnA_ReplyID >= 0 and boardQnA_CommentID = 0 order by boardQnA_ReplyID asc";
+    	        	String SQL2 = "select * from boardQnA where boardQnA_ID = ? and boardQnA_ReplyID >= 0 and boardQnA_CommentID = 0 and available = 1 order by boardQnA_ReplyID asc";
     	        	pstmt2 = conn2.prepareStatement(SQL2);
     	        	pstmt2.setInt(1, rs.getInt("boardQnA_ID"));
     	        	rs2 = pstmt.executeQuery();
@@ -153,6 +364,8 @@ public class BoardQnADBBean {
     	        		tmp.setUserID(rs2.getString(6));
     	        		tmp.setBoardQnA_Reg_Date(rs2.getString(7));
     	        		tmp.setBoardQnA_Content(rs2.getString(8));
+    	        		tmp.setAvailable(rs2.getByte(9));
+    	        		tmp.setBoardQnA_DeleteReg_Date(rs2.getString(10));
     	        		list.add(tmp);
     	        	}
     	        	
@@ -182,7 +395,7 @@ public class BoardQnADBBean {
         ArrayList<BoardQnADataBean> list = new ArrayList<BoardQnADataBean>();
     	try {
     		conn = getConnection();
-    		String SQL = "select * from boardQnA where boardQnA_CommentID = 0 order by boardQnA_ID desc, boardQnA_ReplyID asc";
+    		String SQL = "select * from boardQnA where boardQnA_CommentID = 0 and available = 1 order by boardQnA_ID desc, boardQnA_ReplyID asc";
     		pstmt = conn.prepareStatement(SQL);
     		rs = pstmt.executeQuery();
     		while (rs.next()) {
@@ -195,6 +408,8 @@ public class BoardQnADBBean {
         		tmp.setUserID(rs.getString(6));
         		tmp.setBoardQnA_Reg_Date(rs.getString(7));
         		tmp.setBoardQnA_Content(rs.getString(8));
+        		tmp.setAvailable(rs.getByte(9));
+        		tmp.setBoardQnA_DeleteReg_Date(rs.getString(10));
         		list.add(tmp);
     		}
     	} catch (Exception e) {
@@ -242,6 +457,8 @@ public class BoardQnADBBean {
         		written.setUserID(rs.getString(6));
         		written.setBoardQnA_Reg_Date(rs.getString(7));
         		written.setBoardQnA_Content(rs.getString(8));
+        		written.setAvailable(rs.getByte(9));
+        		written.setBoardQnA_DeleteReg_Date(rs.getString(10));
     		}
     	} catch (Exception e) {
     		e.printStackTrace();
@@ -253,6 +470,45 @@ public class BoardQnADBBean {
     	return written; 
     }
     
+    // 단일 코멘트를 가져오는 메서드
+    public BoardQnADataBean viewComment(int boardQnA_ID, int boardQnA_ReplyID, int boardQnA_CommentID, int boardQnA_CommentID_Re) {
+    	Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+        BoardQnADataBean comment = null;
+    	try {
+    		conn = getConnection();
+    		String SQL = "select * from boardQnA where boardQnA_ID = ? and boardQnA_ReplyID = ? and boardQnA_CommentID = ? and boardQnA_CommentID_Re = ?";
+    		pstmt = conn.prepareStatement(SQL);
+    		pstmt.setInt(1, boardQnA_ID);
+    		pstmt.setInt(2, boardQnA_ReplyID);
+    		pstmt.setInt(3, boardQnA_CommentID);
+    		pstmt.setInt(4, boardQnA_CommentID_Re);
+    		rs = pstmt.executeQuery();
+    		while (rs.next()) {
+    			comment = new BoardQnADataBean();
+        		comment.setBoardQnA_ID(rs.getInt(1));
+        		comment.setBoardQnA_ReplyID(rs.getInt(2));
+        		comment.setBoardQnA_CommentID(rs.getInt(3));
+        		comment.setBoardQnA_CommentID_Re(rs.getInt(4));
+        		comment.setBoardQnA_Title(rs.getString(5));
+        		comment.setUserID(rs.getString(6));
+        		comment.setBoardQnA_Reg_Date(rs.getString(7));
+        		comment.setBoardQnA_Content(rs.getString(8));
+        		comment.setAvailable(rs.getByte(9));
+        		comment.setBoardQnA_DeleteReg_Date(rs.getString(10));
+    		}
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    	} finally {
+    		if (rs != null) try { rs.close(); } catch(SQLException sqle) {}
+    		if (pstmt != null) try { pstmt.close(); } catch(SQLException sqle) {}
+            if (conn != null) try { conn.close(); } catch(SQLException sqle) {}
+    	}
+    	return comment; 
+    }
+    
+    // 코멘트 리스트를 가져오는 메서드
     public ArrayList<BoardQnADataBean> getCommentList(BoardQnADataBean written) {
     	Connection conn = null;
         PreparedStatement pstmt = null;
@@ -260,7 +516,7 @@ public class BoardQnADBBean {
         ArrayList<BoardQnADataBean> list = new ArrayList<BoardQnADataBean>();
     	try {
     		conn = getConnection();
-    		String SQL = "select * from boardQnA where boardQnA_ID = ? and boardQnA_CommentID > 0 and boardQnA_CommentID_Re >= 0 order by boardQnA_CommentID asc, boardQnA_CommentID_Re asc";
+    		String SQL = "select * from boardQnA where boardQnA_ID = ? and boardQnA_CommentID > 0 and boardQnA_CommentID_Re >= 0 and available = 1 order by boardQnA_CommentID asc, boardQnA_CommentID_Re asc";
     		pstmt = conn.prepareStatement(SQL);
     		pstmt.setInt(1, written.getBoardQnA_ID());
     		rs = pstmt.executeQuery();
@@ -274,6 +530,8 @@ public class BoardQnADBBean {
         		tmp.setUserID(rs.getString(6));
         		tmp.setBoardQnA_Reg_Date(rs.getString(7));
         		tmp.setBoardQnA_Content(rs.getString(8));
+        		tmp.setAvailable(rs.getByte(9));
+        		tmp.setBoardQnA_DeleteReg_Date(rs.getString(10));
         		list.add(tmp);
     		}
     	} catch (Exception e) {
