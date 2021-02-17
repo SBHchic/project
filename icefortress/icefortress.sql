68089a1850bb1ec4088513e627dfcab56362b315
baekho Song <ssongqorgh@naver.com>
2021-02-18 00:14:32 +0900

    member 테이블에 server 레코드 추가

--- icefortress/icefortress.sql
+++ icefortress/icefortress.sql
@@ -13,6 +13,8 @@ alter table member modify userPassword varchar(60) not null;
 
 alter table member modify userID varchar(20) not null;
 
+alter table member add server varchar(20);
+
 select * from member;
 
 desc member;
